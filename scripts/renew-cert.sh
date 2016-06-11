fc1621b163f8030283c5f1f98c5fefaabce73b16
Shinichiro Aska <s.aska.org@gmail.com>
2016-06-11 16:25:35 +0900

    Add /etc/init.d/nginx reload

--- scripts/renew-cert.sh
+++ scripts/renew-cert.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# crontab
-# 0 4 8 * * /home/aska/aska.pw/scripts/renew-cert.sh
+# crontab (root)
+# 0 4 1 * * /home/aska/aska.pw/scripts/renew-cert.sh
 
 /usr/local/src/letsencrypt/letsencrypt-auto certonly --webroot -d aska.pw --webroot-path /home/aska/aska.pw/static --renew-by-default
 /home/aska/aska.pw/scripts/ct-submit.sh
+/etc/init.d/nginx reload
